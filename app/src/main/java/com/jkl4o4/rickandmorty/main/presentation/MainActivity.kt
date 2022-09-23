b7552d5f7b8b9e5b9a1e52f848767835125b2b71
jkl4o42 <ruslan.savka4o4@gmail.com>
2022-09-23 18:43:26 +0300

    Characters fragment ui

--- app/src/main/java/com/jkl4o4/rickandmorty/main/presentation/MainActivity.kt
+++ app/src/main/java/com/jkl4o4/rickandmorty/main/presentation/MainActivity.kt
@@ -3,10 +3,14 @@ package com.jkl4o4.rickandmorty.main.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jkl4o4.rickandmorty.R
+import com.jkl4o4.rickandmorty.characters.presentation.CharactersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, CharactersFragment())
+            .commit()
     }
 }
